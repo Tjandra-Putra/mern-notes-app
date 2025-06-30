d787be798a4899fd68badff81aab2a55f91114fc
Tjandra Putra <tjandrap.work@gmail.com>
2025-06-30 11:51:23 +0800

    rate limiter and server completion

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,28 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
-// Middleware to parse JSON bodies
-app.use(express.json());
+// Middleware
+app.use(express.json()); // Parse JSON bodies
+app.use(rateLimiter);
+// app.use((req, res, next) => {
+//   // Middleware to log requests
+//   console.log(`${req.method} request for '${req.url}'`);
+//   next();
+// });
 
 // Master Routes
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server is running on port:", PORT);
+// Once the database is connected, start the server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port:", PORT);
+  });
 });
