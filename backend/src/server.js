0d89b446566a2625faf495c2206f4aef5d1d2e27
Tjandra Putra <tjandrap.work@gmail.com>
2025-07-01 22:36:56 +0800

    production config

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,23 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); // Get the current directory name
 
 // Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Allow requests from the frontend
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // Allow requests from the frontend
+    })
+  );
+}
+
 app.use(express.json()); // Parse JSON bodies
 app.use(rateLimiter);
 
@@ -27,6 +32,16 @@ app.use(rateLimiter);
 
 // Master Routes
 app.use("/api/notes", notesRoutes);
+
+// Serve static files from the frontend build directory (Deployment)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // Serve the index.html file for any other routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 // Once the database is connected, start the server
 connectDB().then(() => {
